f907e9141b6d1a5ca175347d74868617c2375a59
Mateus Prates <mateus7pr@gmail.com>
2024-05-19 19:52:19 -0300

    Audios implementation

--- LikeAMinuteGameJam/Assets/MyGame/Scripts/Audio/PlayerAudios.cs
+++ LikeAMinuteGameJam/Assets/MyGame/Scripts/Audio/PlayerAudios.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour
+public class PlayerAudios : MonoBehaviour
 {
-    AudioSource _audioSource;
-
-    void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
